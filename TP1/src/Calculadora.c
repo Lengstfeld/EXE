ae004e244d4332d27883e1fcb0a5c7098e5022a2
Lengstfeld <lengstfeld@gmail.com>
2020-09-06 04:52:02 -0300

    Calculadora

--- TP1/src/Calculadora.c
+++ TP1/src/Calculadora.c
@@ -10,13 +10,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-int logica(int A, int B);
-int logicaSuma(int vA, int vB);
-int logicaResta(int vA, int vB);
-int logicaDivision(int vA, int vB);
-int logicaMultiplicacion(int vA, int vB);
-int logicaFactorial(int vA, int vB);
+#include "Logica.h"
 
 int main(void){
 	int A=0;
@@ -27,49 +21,9 @@ int main(void){
 	scanf("%d", &A);
 	printf("Ingrese valor de B: ");
 	scanf("%d", &B);
-	printf("\n\n*********************");
+	printf("\n*********************");
 	logica(A,B);
 	return EXIT_SUCCESS;
 }
-int logica(int A, int B){
-	logicaSuma(A,B);
-	logicaResta(A,B);
-	logicaDivision(A,B);
-	logicaMultiplicacion(A,B);
-	logicaFactorial(A,B);
-	return 0;
-}
 
-int logicaSuma(int vA, int vB){
-	int suma=0;
-	suma=vA+vB;
-	printf("\nEl resultado de la suma es: %.0f", suma);
-	return 0;
-}
-int logicaResta(int vA, int vB){
-	int resta=0;
-	resta=vA-vB;
-	printf("\nEl resultado de la resta es: %d", resta);
-	return 0;
-}
-int logicaDivision(int vA, int vB){
-	int division=0;
-	division=vA/vB;
-	printf("\nEl resultado de la division es: %d", division);
-	return 0;
-}
-int logicaMultiplicacion(int vA, int vB){
-	int multiplicacion=0;
-	multiplicacion=vA*vB;
-	printf("\nEl resultado de la multiplicacion es: %d", multiplicacion);
-	return 0;
-}
-int logicaFactorial(int vA, int vB){
-	int factorialA=0;
-	int factorialB=0;
-	factorialA=vA.(vA-1);
-	printf("\nEl resultado del factorial de A es: %d", factorialA);
-	printf("\nEl resultado del factorial de B es: %d", factorialB);
-	return 0;
-}
 
---
+++ TP1/src/Logica.h
@@ -0,0 +1,15 @@
+/*
+ * Logica.h
+ *
+ *  Created on: 6 sep. 2020
+ *      Author: lengs
+ */
+
+#ifndef LOGICA_H_
+#define LOGICA_H_
+
+
+#endif /* LOGICA_H_ */
+
+
+int logica(int vA, int vB);
