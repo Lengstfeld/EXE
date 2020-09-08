89b245e9e140345dfbdcfb8f49e247401979914c
Lengstfeld <lengstfeld@gmail.com>
2020-09-08 02:28:06 -0300

    Calculadora

--- TP1/src/Calculadora.c
+++ TP1/src/Calculadora.c
@@ -22,45 +22,76 @@ int main(void){
 	int resultadoMultiplicacion=0;
 	int resultadoFactorialA=0;
 	int resultadoFactorialB=0;
+	char salir='n';
+	int menu;
+
 	printf("*****Calculadora*****");
-	printf("\n\nIngrese valor de A: ");
-	scanf("%d", &A);
-	printf("Ingrese valor de B: ");
-	scanf("%d", &B);
-	printf("\n*********************");
-	resultadoSuma = logicaSuma(A,B);
-	resultadoResta = logicaResta(A,B);
-	resultadoDivision = logicaDivision(A,B);
-	resultadoMultiplicacion = logicaMultiplicacion(A,B);
-	resultadoFactorialA = logicaFactorialA(A,B);
-	resultadoFactorialB = logicaFactorialB(A,B);
-	printf("\n\nEl resultado de la suma es: %d", resultadoSuma);
-	printf("\nEl resultado de la resta es: %d", resultadoResta);
-	if(A!=0 && B!=0)
-	{
-		printf("\nEl resultado de la division es: %.2f", resultadoDivision);
-	}
-	else
-	{
-		printf("\nNo se puede divir entre 0");
-	}
-	printf("\nEl resultado de la multiplicacion es: %d", resultadoMultiplicacion);
-	if(A==0)
-	{
-		printf("\nFactorial de A es 0");
-	}
-	else if(A!=0)
-	{
-		printf("\nEl resultado del factorial de A es: %d", resultadoFactorialA);
-	}
-	if(B==0)
-	{
-		printf("\nFactorial de B es 0");
-	}
-	else if(B!=0)
-	{
-		printf("\nEl resultado del factorial de B es: %d", resultadoFactorialB);
-	}
+	do{
+		printf("\n\n1)Ingresar 1er operando, valor actual: %d", A);
+		printf("\n2)Ingresar 2do operando, valor actual: %d", B);
+		printf("\n3)Calcular todas las operaciones");
+		printf("\n4)Informar resultados");
+		printf("\n5)Salir");
+		printf("\n\n*********************");
+		printf("\n\nIngrese la opcion que desea usar: ");
+		scanf("%d", &menu);
+		switch(menu){
+		case 1:
+			printf("\n\nIngrese valor de A: ");
+			scanf("%d", &A);
+			break;
+		case 2:
+			printf("\n\nIngrese valor de B: ");
+			scanf("%d", &B);
+			break;
+		case 3:
+			resultadoSuma = logicaSuma(A,B);
+			resultadoResta = logicaResta(A,B);
+			resultadoDivision = logicaDivision(A,B);
+			resultadoMultiplicacion = logicaMultiplicacion(A,B);
+			resultadoFactorialA = logicaFactorialA(A,B);
+			resultadoFactorialB = logicaFactorialB(A,B);
+			break;
+		case 4:
+			printf("\n\nEl resultado de la suma es: %d", resultadoSuma);
+			printf("\nEl resultado de la resta es: %d", resultadoResta);
+			if(A!=0 && B!=0)
+			{
+				printf("\nEl resultado de la division es: %.2f", resultadoDivision);
+			}
+			else
+			{
+				printf("\nNo se puede divir entre 0");
+			}
+			printf("\nEl resultado de la multiplicacion es: %d", resultadoMultiplicacion);
+			printf("\nEl resultado del factorial de A es: %d", resultadoFactorialA);
+			printf("\nEl resultado del factorial de B es: %d", resultadoFactorialB);
+			break;
+		case 5:
+			do{
+				printf("\n\nEsta seguro que quiere salir? s/n :");
+				fflush(stdin);
+				scanf("%c", &salir);
+			}while(salir!='n' && salir!='s');
+			break;
+		default:
+			printf("\n\nError, opcion inexistente\n\n");
+			system("pause");
+			break;
+		}
+		system("CLS");
+		printf("\n*********************");
+	}while(salir=='n');
+	system("CLS");
+	printf("\n**********!***********");
+	printf("\n**********G***********");
+	printf("\n**********R***********");
+	printf("\n**********A***********");
+	printf("\n**********C***********");
+	printf("\n**********I***********");
+	printf("\n**********A***********");
+	printf("\n**********S***********");
+	printf("\n**********!***********");
 	return EXIT_SUCCESS;
 }
 
